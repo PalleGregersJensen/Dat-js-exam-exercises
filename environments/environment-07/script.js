d2c7a16b824df0ad9366681fcbf6a1e2f8b3bd20
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-30 16:30:07 +0200

    Update script.js

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let students = [];
+
+function start() {
+  console.log("JS kører");
+  document.querySelector("#create-student-form").addEventListener("submit", createStudentObject);
+}
+
+function createStudentObject(event) {
+  event.preventDefault();
+  const form = event.target;
+
+  const name = form.name;
+  const age = form.age;
+  const email = form.email;
+  students.push(newStudent);
+    console.log(students);
+    
+}
+
+function handleCreateButton() {}
