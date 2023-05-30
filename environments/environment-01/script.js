8bbe9707cd3f90e3cbd4fb35bf7f4f424f41afbc
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-30 20:55:58 +0200

    Del 1 og 2 af opgave er løst. Del tre fungerer ikke.

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,44 @@
+// # Øvelse 1
+
+// Environment: `environment-01`
+
+// 1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en variabel.
+// 2. Lav en funktion der viser listen på websiden - hver user skal vises med navn og hvorvidt de er aktive eller ej.
+// 3. Filtrér listen så den kun viser admin-brugere.
+
 "use strict";
 
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start() {
+  console.log("JS kører");
+  users = await getJsonFile();
+  console.log(users);
+  users.forEach(showUsers);
+  showAdmins();
+}
+
+async function getJsonFile() {
+  const response = await fetch("users.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showUsers(user) {
+  const usersHtml = /*html*/ `<li>${user.name} - ${user.active}</li>`;
+  document.querySelector("#user-count").insertAdjacentHTML("beforeend", usersHtml);
+
+  const admins = users.filter(showAdmins(user));
+  console.log(admins);
+}
+
+function showAdmins() {
+  const admins = users.filter(function (user) {
+    return user.role === "admin";
+  });
+  console.log(admins);
+}
