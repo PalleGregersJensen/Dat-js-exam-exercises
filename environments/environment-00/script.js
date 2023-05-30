3bd8d744ade50e646b4d056f589d14ffd07ecca3
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-30 19:39:31 +0200

    Opgave 0 lavet færdig

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,26 @@
+// Environment: `environment-00`
+
+// 1. Lav en funktion `clicked`, der udskriver "Det virker" i konsollen, når man trykker på knappen på HTML-siden.
+// 2. Udvid funktionen så den skjuler teksten `#result_failure` og viser teksten `#result_success` ved hjælp af klasserne `hide` og `show`.
+// 3. Tilføj en funktion der automatisk skjuler begge tekster så snart siden loades - sørg for at knappen stadig virker og viser den rette tekst.
+
 "use strict";
+
+window.addEventListener("load", start);
+
+function start() {
+    console.log("JS kører");
+    hideText();
+    document.querySelector("button").addEventListener("click", clicked);
+}
+
+function clicked() {
+    console.log("Det virker");
+    document.querySelector("#result_failure").classList.add("hide");
+    document.querySelector("#result_success").classList.add("show");
+}
+
+function hideText() {
+    document.querySelector("#result_failure").classList.add("hide");
+    document.querySelector("#result_success").classList.add("hide");
+}
