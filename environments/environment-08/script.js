7c01183693729c5f4de3b93ec3e6027ee050eb25
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-05-31 19:25:22 +0200

    Del 1 og 2 lavet.

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+window.addEventListener("load", start);
+
+let jsonData = [];
+
+async function start() {
+  console.log("JS kører");
+  jsonData = await getJsonFile();
+    console.log(jsonData);
+    jsonData.forEach(showSongsOnWebside);
+    
+}
+
+async function getJsonFile() {
+  const response = await fetch("playlist.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showSongsOnWebside(song) {
+    const songsHtml = /*html*/ `<li>${song.title} <button>Remove</button></li>`;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", songsHtml);
+    document.querySelector("#songlist li:last-child button").addEventListener("click", removeSongFromList);
+}
+
+function removeSongFromList() {
+    console.log("Button clciked"); 
+}
+
+// Kan laves med filter. Tænk omvendt. Dem, der er markeret, skal slettes.
