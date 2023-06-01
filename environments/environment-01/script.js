85f4837fef2ef630532f75a5566b737ab0e452a1
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-01 12:46:18 +0200

    Opgavedel 1 og 2 er lavet.

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,35 @@
+// # Øvelse 4
+
+// Environment: `environment-01`
+
+// 1. Lav en funktion der indlæser JSON-filen `users.json` og gemmer listen i en global variabel.
+// 2. Lav en funktion der viser listen på websiden - vis kun aktive brugere.
+// 3. Lav en funktion der modtager `name`, `active` og `role`, opretter et `user` objekt med de tre properties, og tilføjer objektet til den globale liste. Listen på websiden opdateres hver gang, der oprettes et nyt objekt.
+
 "use strict";
 
+window.addEventListener("load", start);
+
+let users = [];
+
+async function start() {
+  console.log("JS kører");
+  users = await getJsonData();
+    console.log(users);
+    users.forEach(showUsersOnWebsite);
+}
+
+async function getJsonData() {
+  const response = await fetch("users.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showUsersOnWebsite(user) {
+    if(user.active === true) {
+  const userHtml = /*html*/ `<li>${user.name} - ${user.active}</li>`;
+  document.querySelector("#userlist").insertAdjacentHTML("beforeend", userHtml);
+}
+}
