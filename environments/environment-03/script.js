a2a14e72853ec3999b21a7f9023b983f8a17b31f
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-02 15:34:11 +0200

    Del tre virker nu. Stock og inStock drillede.

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -16,42 +16,50 @@ let products = [
 
 window.addEventListener("load", start);
 
-// let prdoucts = [{ name: "beer", price: 20, inStock: true }, { name: flower, price: 15, inStock: true }, {name: apples, price: 25, inStock: false}]
-
 function start() {
   console.log("JS kører");
   console.log(products);
-    showProducts(products);
-    document.querySelector("#create-form").addEventListener("submit", addProdctToArray);
+  showProducts(products);
+  document.querySelector("#create-form").addEventListener("submit", addProdctToArray);
 }
 
-function showProducts(product) {
-  for (const product of products)
-    if (product.inStock) {
-      { const productHtml = /*html*/ `<p>${product.name}-${product.price}-${product.inStock}</p>`;
+function showProducts(productList) {
+  document.querySelector("#list-container").innerHTML = "";
+  for (const product of productList)
+      if (product.inStock === true) {
+      {
+        const productHtml = /*html*/ `<p>${product.name}-${product.price}-${product.inStock}</p>`;
         document.querySelector("#list-container").insertAdjacentHTML("beforeend", productHtml);
       }
-    }
+       }
 }
 
 function addProdctToArray() {
-    event.preventDefault();
-    console.log("button clicked");
-    const form = event.target;
-    const name = form.elements["product-name"].value;
-    const price = form.elements["product-price"].value;
-    const inStock = form.stock.checked;
-    const newProduct = returnObject(name, price, inStock);
-    console.log(newProduct);
-    products.push(newProduct);
-    console.log(products);
+  event.preventDefault();
+  console.log("button clicked");
+  const form = event.target;
+  const name = form.elements["product-name"].value;
+  let price = document.querySelector("#product-price").value;
+  let stock = document.querySelector("#stock").checked;
+  price = Number(price);
+  stock = Boolean(stock);
+  console.log(name);
+  console.log(price);
+  console.log(stock);
+  const newProduct = returnObject(name, price, stock);
+  console.log(newProduct);
+  products.push(newProduct);
+  console.log(products);
+  showProducts(products);
+  form.reset();
 }
 
-function returnObject() {
-    const product = {
-        name: name,
-        price: price,
-        inStock: inStock,
-    }
-    return product;
+function returnObject(name, price, stock) {
+  const product = {
+    name: name,
+    price: price,
+    inStock: stock,
+  };
+  return product;
 }
+
