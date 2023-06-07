29cea0f74212f9b33d95ce909f6526706bcb71ee
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-07 13:25:30 +0200

    Del 1, 2 og 3 er lavet.

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -6,7 +6,6 @@
 // 2. Lav en funktion der viser listen på websiden - hver user skal vises med navn og hvorvidt de er aktive eller ej.
 // 3. Filtrér listen så den kun viser admin-brugere.
 
-
 "use strict";
 
 let users = [];
@@ -14,36 +13,37 @@ let users = [];
 window.addEventListener("load", start);
 
 async function start() {
-    console.log("js kører")
-    users = await getJsonData();
-    console.log(users);
-    filterByAdmin(users);
-    showUsers(users);
+  console.log("js kører");
+  users = await getJsonData();
+  console.log(users);
+  const filteredUsers = filterByAdmin(users);
+  showUsers(filteredUsers);
 }
 
 async function getJsonData() {
-    const response = await fetch("users.json");
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    return data;
+  const response = await fetch("users.json");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 function showUsers(userList) {
-    document.querySelector("#userlist").innerHTML = "";
+  document.querySelector("#userlist").innerHTML = "";
   for (const user of userList) {
     const userHtml = /*html*/ `<li>Name: ${user.name} - Active: ${user.active}</li>`;
     document.querySelector("#userlist").insertAdjacentHTML("beforeend", userHtml);
   }
 }
 
-function filterByAdmin() {
-    users.filter(checkAdmin);
+function filterByAdmin(users) {
+  const adminUsers = users.filter(checkAdmin);
+    console.log(adminUsers);
+    return adminUsers;
 }
-    function checkAdmin(user) {
-        if (user.role === "admin") {
-            console.log(users);
-            return;
-            
-        }
-    }
+function checkAdmin(user) {
+  if (user.role === "admin") {
+    console.log(users);
+    return true;
+  }
+}
