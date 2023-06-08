60eb30244973058dfaba9c8c9e4db53cfccbb377
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-08 14:45:36 +0200

    Delopgave 1, 2 og 3 er lavet.

--- environments/environment-03/script.js
+++ environments/environment-03/script.js
@@ -7,3 +7,42 @@
 // 3. Lav en funktion der kan sortere listen efter `name`, `price` og `inStock`. Sorteringen skal ske på baggrund af valgte `option` i `select` (se `environment-03`). Sortering ændrer sig hver gang en ny sortering `option` vælges.
 
 "use strict";
+
+const products = [
+  { name: "beer", price: 25, inStock: true },
+  { name: "whisky", price: 125, inStock: false },
+  { name: "apples", price: 10, inStock: true },
+];
+
+window.addEventListener("load", start);
+
+function start() {
+  console.log("JS kører");
+    console.log(products);
+    showProducts(products);
+    document.querySelector("#select-sort-by").addEventListener("change", handleSortBy);
+}   
+
+
+function showProducts(productList) {
+    document.querySelector("#list-container").innerHTML = "";
+  for (const product of productList) {
+    const productHtml = /*html*/ `<p>${product.name} - ${product.price} - ${product.inStock}</p>`;
+    document.querySelector("#list-container").insertAdjacentHTML("beforeend", productHtml);
+  }
+}
+
+
+function handleSortBy() {
+    console.log("change");
+    const sortBy = document.querySelector("#select-sort-by").value;
+    console.log(sortBy);
+    if (sortBy === "name") {
+        products.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === "price") {
+        products.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "inStock") {
+        products.sort((a, b) => b.inStock - a.inStock);
+    }
+    showProducts(products);
+}
