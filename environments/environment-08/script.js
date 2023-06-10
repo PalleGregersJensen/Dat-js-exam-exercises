1f648f00ff0abf1deee18d45a370b528025e9d45
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-06-10 22:26:35 +0200

    Delopgave 1 og 2 er løst. Delopgave 3 er  ikke løst.

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -9,3 +9,34 @@
 // 3. Få remove-knappen til at fjerne den pågældende sang fra listen, og udskriv listen igen.
 
 "use strict";
+
+let playlist = [];
+
+window.addEventListener("load", start);
+
+async function start() {
+    console.log("JS kører");
+    playlist = await getJsonData();
+    console.log(playlist);
+    showPlaylist(playlist);
+}
+
+async function getJsonData() {
+    const response = await fetch("playlist.json");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+function showPlaylist(songList) {
+    for(const song of songList) {
+    const songHtml = /*html*/ `<li>${song.artist} - ${song.title} <button>Remove</button></li>`;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", songHtml)
+    }
+    document.querySelector("#songlist li:last-child button").addEventListener("click", removeSong);
+}
+
+function removeSong() {
+    console.log("remove song");
+}
